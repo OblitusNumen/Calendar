b285f96fa6b5106af239536282474ac4b921115e
OblitusNumen <o.aleksashkin@yandex.ru>
2024-10-17 19:01:07 +0300

    add weekday names

--- app/src/main/java/oblitusnumen/calendar/ui/model/tab/CalendarTab.kt
+++ app/src/main/java/oblitusnumen/calendar/ui/model/tab/CalendarTab.kt
@@ -6,10 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.stopScroll
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -17,6 +14,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
@@ -40,15 +38,27 @@ class CalendarTab : Tab, Functional, TopBarModifier {
 
     @Composable
     override fun compose(calendarViewModel: MainActivity.CalendarViewModel) {
-        val now = LocalDate.now()
-        if (calendarLazyListState == null) calendarLazyListState = rememberLazyListState(Int.MAX_VALUE / 2)
-        LazyColumn(
-            state = calendarLazyListState!!,
-            modifier = Modifier
-        ) {
-            items(Int.MAX_VALUE, itemContent = {
-                DisplayMonth(now.withDayOfMonth(1).plusMonths((it - Int.MAX_VALUE / 2).toLong()), calendarViewModel)
-            })
+        Column {
+            Row {
+                var i = 1;
+                while (i <= 7) {
+                    Box(Modifier.width(getWidthPartIncludePadding(7f)).height(getWidthPartIncludePadding(14f))
+                        .border(2.dp, MaterialTheme.colorScheme.primary)) {
+                        Text(stringArrayResource(R.array.weekdayNames)[i-1], Modifier.align(Alignment.Center))
+                    }
+                    i++;
+                }
+            }
+            val now = LocalDate.now()
+            if (calendarLazyListState == null) calendarLazyListState = rememberLazyListState(Int.MAX_VALUE / 2)
+            LazyColumn(
+                state = calendarLazyListState!!,
+                modifier = Modifier
+            ) {
+                items(Int.MAX_VALUE, itemContent = {
+                    DisplayMonth(now.withDayOfMonth(1).plusMonths((it - Int.MAX_VALUE / 2).toLong()), calendarViewModel)
+                })
+            }
         }
     }
 
